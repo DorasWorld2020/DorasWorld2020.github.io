e3d72afb13f6907448f6213bee3f61e5addc22a1
DorasWorld2020 <panxl791@163.com>
2020-12-04 16:43:30 +0100

    Site updated: 2020-12-04 16:43:30

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({  
+container: document.getElementById('aplayer'),  
+fixed: true,  
+autoplay: true,  
+audio: [  
+{  
+name: '世间美好与你环环相扣',  
+artist: '柏松',  
+url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',  
+// cover: '/images/3.jpg',  
+cover: '/images/player.jpg',
+} 
+]  
+});  
