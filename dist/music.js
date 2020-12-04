f871c8cf0886c980026cbc2547c5108a78396b8a
DorasWorld2020 <panxl791@163.com>
2020-12-04 17:10:51 +0100

    Site updated: 2020-12-04 17:10:51

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,12 @@ fixed: true,
 autoplay: true,  
 audio: [  
 {  
+name: '你看不见',  
+artist: 'Rainbow计划',  
+url: 'http://music.163.com/song/media/outer/url?id=35625825.mp3',  
+cover: '/images/player1.png',  
+}, 
+{  
 name: '世间美好与你环环相扣',  
 artist: '柏松',  
 url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',  
